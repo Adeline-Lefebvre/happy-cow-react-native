7a1cedf368d9e5464cb3915e15930f5cc941ed82
Adeline Lefebvre <adelinelefebvre@iphone-de-adeline.home>
2021-08-16 13:21:04 +0200

    message

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons, Feather } from "@expo/vector-icons";
+import SplashScreen from "./containers/Splashscreen";
+import RestaurantsScreen from "./containers/RestaurantsScreen";
+import RestaurantScreen from "./containers/RestaurantScreen";
+import MapScreen from "./containers/MapScreen";
+import FavoritesScreen from "./containers/FavoritesScreen";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  // const [isLoading, setIsLoading] = useState(true);
+  const isLoading = false;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {isLoading ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Splash"
+            component={SplashScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="Tab" options={{ headerShown: false }}>
+            {() => (
+              <Tab.Navigator
+                tabBarOptions={{
+                  activeTintColor: "purple",
+                  incativeTintColor: "gray",
+                }}
+              >
+                <Tab.Screen
+                  name="Explore"
+                  options={{
+                    tabBarLabel: "Explorer",
+                    tabBarIcon: ({ color }) => (
+                      <Ionicons name="search" size={24} color={color} />
+                    ),
+                  }}
+                >
+                  {() => (
+                    <Stack.Navigator>
+                      <Stack.Screen
+                        name="Restaurants"
+                        component={RestaurantsScreen}
+                      />
+                      <Stack.Screen
+                        name="Restaurant"
+                        component={RestaurantScreen}
+                      />
+                      <Stack.Screen name="Map" component={MapScreen} />
+                    </Stack.Navigator>
+                  )}
+                </Tab.Screen>
+                <Tab.Screen
+                  name="Favorites"
+                  options={{
+                    tabBarLabel: "Favoris",
+                    tabBarIcon: ({ color }) => (
+                      <Feather name="heart" size={24} color={color} />
+                    ),
+                  }}
+                >
+                  {() => (
+                    <Stack.Navigator>
+                      <Stack.Screen
+                        name="Favorites"
+                        component={FavoritesScreen}
+                      />
+                    </Stack.Navigator>
+                  )}
+                </Tab.Screen>
+              </Tab.Navigator>
+            )}
+          </Stack.Screen>
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
