077534f0ae2c3255436d9aecab3d3b3155caf910
Adeline Lefebvre <adelinelefebvre@iphone-de-adeline.home>
2021-08-16 18:58:50 +0200

    message

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -2,7 +2,24 @@ import React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
 
 const RestaurantCard = ({ item }) => {
-  const { thumbnail, name, rating, description } = item;
+  const { thumbnail, name, type, rating, description } = item;
+
+  if (type === "Veg Store") {
+    const icon = require(`../assets/Icons/Veg-Store.png`);
+  } else if (type === "Health Store") {
+    const icon = require(`../assets/Icons/Health-Store.png`);
+  } else if (type === "Ice Cream") {
+    const icon = require(`../assets/Icons/Ice-Cream.png`);
+  } else if (type === "Juice Bar") {
+    const icon = require(`../assets/Icons/Juice-Bar.png`);
+  } else if (type === "Food Truck") {
+    const icon = require(`../assets/Icons/Food-Truck.png`);
+  } else if (type === "Market Vendor") {
+    const icon = require(`../assets/Icons/Market-Vendor.png`);
+  } else {
+    const icon = require(`../assets/Icons/${type}.png`);
+  }
+
   return (
     <View style={styles.restaurantCard}>
       <Image source={{ uri: thumbnail }} style={styles.image} />
@@ -12,6 +29,10 @@ const RestaurantCard = ({ item }) => {
           {description}
         </Text>
       </View>
+      <Image
+        source={require("../assets/Icons/Veg-Store.png")}
+        style={styles.icon}
+      />
     </View>
   );
 };
@@ -36,5 +57,12 @@ const styles = StyleSheet.create({
   description: {
     position: "absolute",
     bottom: 0,
+  },
+  icon: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: 20,
+    height: 20,
   },
 });
